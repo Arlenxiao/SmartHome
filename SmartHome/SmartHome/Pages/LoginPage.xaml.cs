99357bb0f603425eb73e5581f2649021c00965c0
arlenxiao <yuqicook@163.com>
2015-04-25 09:15:58 +0800

    简单实现登录跳转主页

--- SmartHome/SmartHome/Pages/LoginPage.xaml.cs
+++ SmartHome/SmartHome/Pages/LoginPage.xaml.cs
@@ -21,11 +21,23 @@ namespace SmartHome.Pages
         public LoginPage()
         {
             InitializeComponent();
+            //this.account.Style = BaseStyle.entryStyle;
+            //this.password.Style = BaseStyle.entryStyle;
         }
 
         public async void Login(object sender, EventArgs e)
         {
-            await this.DisplayAlert("警告", "账号:"+this.account.Text+" 密码"+this.password.Text, "OK");
+
+            var user = this.account.Text;
+            var pwd = this.password.Text;
+            if (user == "1" && pwd == "1")
+            {
+                await this.Navigation.PushModalAsync(new MainPage());
+            }
+            else
+            {
+                await this.DisplayAlert("警告", "账号:" + user + " 密码:" + pwd , "OK");
+            }
         }
 
         public async void Register(object sender, EventArgs e)
---
+++ SmartHome/SmartHome/Style/BaseStyle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SmartHome
+{
+    public class BaseStyle : Page
+    {
+        /// <summary>
+        /// 按钮样式
+        /// </summary>
+        public static Style buttonStyle = new Style(typeof(Button))
+        {
+            Setters =
+                {
+                    new Setter {Property = BackgroundColorProperty, Value = Color.Transparent},
+                    new Setter {Property = HeightRequestProperty, Value = 80},
+                    new Setter {Property = Button.BorderWidthProperty, Value = 1},
+                    new Setter {Property = Button.BorderColorProperty, Value = Color.White},
+                    new Setter {Property = Button.TextColorProperty, Value = Color.White},
+                }
+        };
+
+        public static Style entryStyle = new Style(typeof(Entry))
+        {
+            Setters =
+                {
+                    new Setter {Property = BackgroundColorProperty, Value = Color.Transparent},
+                    new Setter {Property = HeightRequestProperty, Value = 80},
+                    new Setter {Property = Entry.TextColorProperty, Value = Color.White},
+                }
+        };
+
+        public static Style lableStyle = new Style(typeof(Label))
+        {
+            Setters =
+                {
+                    new Setter {Property = BackgroundColorProperty, Value = Color.Transparent},
+                    new Setter {Property = HeightRequestProperty, Value = 80},
+                    new Setter {Property = WidthRequestProperty, Value = 120},
+                    new Setter {Property = Label.TextColorProperty, Value = Color.White},
+                }
+        };
+    }
+}
